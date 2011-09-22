1017fe4ec081afc3a7941384ce6ddd87ab41d5dc
Josh Wright <jshwright@gmail.com>
2011-09-22 12:45:28 -0400

    Formatting change and comment removal, no functional change

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,19 +1,13 @@
 class ssh {
-    package { "openssh-server":     ensure  => installed }
+    package { "openssh-server":
+        ensure  => installed
+    }
 
     file { "sshdconfig":
         name    => "/etc/ssh/sshd_config",
         content => template("ssh/sshd_config.erb"),
         owner   => "root",
     }
-
-#    ssh_authorized_key { "rootkey":
-#        ensure  => present,
-#        type    => ssh-rsa,
-#        key     => file("/etc/puppet/files/authorized_keys/rootkey"),
-#        name    => "root@brianjhenderson.com",
-#        user    => root,
-#    }
 
     service { "ssh":
         require => Package[openssh-server],
